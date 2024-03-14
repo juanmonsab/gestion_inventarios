8904d04ac4e6918563f39dc63234ed69ccb24b55
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2024-03-13 23:00:45 -0500

    Configura y hace las importaciones necesarias en el archivo GestionInventariosApplication

--- src/main/java/co/edu/unisabana/api/gestioninventarios/GestionInventariosApplication.java
+++ src/main/java/co/edu/unisabana/api/gestioninventarios/GestionInventariosApplication.java
@@ -2,12 +2,16 @@ package co.edu.unisabana.api.gestioninventarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"co.edu.unisabana.prueba.gestioninventarios.repositorio"})
+@ComponentScan(basePackages = {"co.edu.unisabana.prueba.gestioninventarios"})
+@EntityScan(basePackages = {"co.edu.unisabana.prueba.gestioninventarios.modelo"})
 public class GestionInventariosApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GestionInventariosApplication.class, args);
 	}
-
 }
