39813ec342fba398846930cd8ca945c5751bf9db
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2024-03-13 22:46:52 -0500

    Commit inicial

---
+++ src/main/java/co/edu/unisabana/api/gestioninventarios/GestionInventariosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.api.gestioninventarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionInventariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionInventariosApplication.class, args);
+	}
+
+}
