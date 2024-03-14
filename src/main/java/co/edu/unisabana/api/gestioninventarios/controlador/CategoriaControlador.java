6e4ba6f1f584948466eede4f7bdf6cc3f90ff1d2
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2024-03-14 08:31:38 -0500

    Agrega el controlador REST para Categoria

---
+++ src/main/java/co/edu/unisabana/api/gestioninventarios/controlador/CategoriaControlador.java
@@ -0,0 +1,33 @@
+package co.edu.unisabana.api.gestioninventarios.controlador;
+
+import co.edu.unisabana.api.gestioninventarios.dto.CategoriaDTO;
+import co.edu.unisabana.api.gestioninventarios.servicio.ServicioCategoria;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaControlador {
+
+    private final ServicioCategoria servicioCategoria;
+
+    public CategoriaControlador(ServicioCategoria servicioCategoria) {
+        this.servicioCategoria = servicioCategoria;
+    }
+
+    @GetMapping
+    public List<CategoriaDTO> obtenerCategorias() {
+        return servicioCategoria.obtenerTodasLasCategorias();
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoriaDTO> agregarCategoria(@RequestBody CategoriaDTO categoriaDTO) {
+        CategoriaDTO nuevaCategoria = servicioCategoria.agregarCategoria(categoriaDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaCategoria);
+    }
+}
