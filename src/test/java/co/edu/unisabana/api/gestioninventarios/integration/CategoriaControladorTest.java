3640cf37248247b677489198d215f744cc47cd46
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2024-05-19 15:55:18 -0500

    Agrega las pruebas de integración funcionales

--- src/test/java/co/edu/unisabana/api/gestioninventarios/integration/CategoriaControladorTest.java
+++ src/test/java/co/edu/unisabana/api/gestioninventarios/integration/CategoriaControladorTest.java
@@ -1,30 +1,70 @@
 package co.edu.unisabana.api.gestioninventarios.integration;
+
 import co.edu.unisabana.api.gestioninventarios.dto.CategoriaDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-
-import static org.assertj.core.api.Fail.fail;
-
-
-public class CategoriaControladorTest extends AbstractTest {
-    String path = "/categorias";
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CategoriaControladorTest {
 
     @Autowired
-    private DataSource dataSource;
+    private TestRestTemplate restTemplate;
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    @Test
+    public void testAgregarCategoria() {
+        CategoriaDTO categoriaDTO = new CategoriaDTO();
+        categoriaDTO.setNombre("CategoriaTest");
 
+        ResponseEntity<CategoriaDTO> responseAgregar =
+                restTemplate.postForEntity("/categorias/agregar", categoriaDTO, CategoriaDTO.class);
 
+        Assertions.assertNotNull(responseAgregar.getBody());
+        Assertions.assertEquals(HttpStatus.CREATED, responseAgregar.getStatusCode());
+        Assertions.assertEquals("Nueva Categoría", responseAgregar.getBody().getNombre());
+    }
+
+    @Test
+    public void testObtenerCategorias() {
+        ResponseEntity<CategoriaDTO[]> responseObtener =
+                restTemplate.getForEntity("/categorias/obtener", CategoriaDTO[].class);
+
+        CategoriaDTO[] categorias = responseObtener.getBody();
+        Assertions.assertNotNull(categorias);
+        Assertions.assertEquals(HttpStatus.OK, responseObtener.getStatusCode());
+    }
+
+    @Test
+    public void testEliminarCategoria() {
+        CategoriaDTO categoriaDTO = new CategoriaDTO();
+        categoriaDTO.setNombre("Categoría a Eliminar");
+
+        ResponseEntity<CategoriaDTO> responseAgregar =
+                restTemplate.postForEntity("/categorias/agregar", categoriaDTO, CategoriaDTO.class);
+
+        Assertions.assertNotNull(responseAgregar.getBody());
+        Assertions.assertEquals(HttpStatus.CREATED, responseAgregar.getStatusCode());
+
+        restTemplate.delete("/categorias/eliminar/" + responseAgregar.getBody().getId());
+
+        ResponseEntity<CategoriaDTO[]> responseEliminar =
+                restTemplate.getForEntity("/categorias/obtener", CategoriaDTO[].class);
+
+        CategoriaDTO[] categoriasDespuesEliminar = responseEliminar.getBody();
+        if (categoriasDespuesEliminar != null) {
+            for (CategoriaDTO cat : categoriasDespuesEliminar) {
+                Assertions.assertNotEquals(responseAgregar.getBody().getId(), cat.getId(),
+                        "Se encontró la categoría eliminada en la lista después de la eliminación");
+            }
+        }
+    }
 }
+
+
 
 
 
